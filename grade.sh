bddb5749cf60e6f46d380e39bb7d672c7d1363b4
nnorstad <nnorstad@ucsd.edu>
2023-05-11 15:26:24 -0700

    pe

--- grade.sh
+++ grade.sh
@@ -8,9 +8,23 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [[ -f student-submission/ListExamples.java ]]
+then
+    echo 'file found'
+else
+    echo 'file not found'
+    exit 1
+fi
 
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
+cp student-submission/ListExamples.java grading-area
+cp TestListExamples.java grading-area
+cp -r lib grading-area
 
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
+javac -cp CPATH grading-area/*.java 2> err-output.txt
+if [[ $? != 0 ]]
+then
+    echo 'compile error'
+    exit 1
+fi
+
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore grading-area/TestListExamples > junit-out.txt
