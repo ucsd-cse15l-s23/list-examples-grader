e3ae3bedba83d454aec6fe3b5511a93560073911
Alexander Zhou <alexzhou3999@gmail.com>
2023-05-11 18:45:49 -0700

    stuff

--- TestListExamples.java
+++ TestListExamples.java
@@ -18,6 +18,41 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+
+  @Test 
+  public void testMerge_differentLengths() {
+    List<String> left = Arrays.asList("Cheetos", "Goldfish", "Cheeseits");
+    List<String> right = Arrays.asList("Donald");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("Cheeseits", "Cheetos", "Donald", "Goldfish");
+    assertEquals(expected, merged);
+  }
+  @Test 
+  public void testMerge_differentLength2() {
+    List<String> right = Arrays.asList("Cheetos", "Goldfish", "Cheeseits");
+    List<String> left = Arrays.asList("Donald");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("Cheeseits", "Cheetos", "Donald", "Goldfish");
+    assertEquals(expected, merged);
+  }
+
+  @Test 
+  public void testMerge_capitalization() {
+    List<String> left = Arrays.asList("a", "b", "c");
+    List<String> right = Arrays.asList("A", "B", "C");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("A", "B", "C", "a", "b", "c");
+    assertEquals(expected, merged);
+  }
+
+  @Test
+  public void testMerge_duplicates() {
+    List<String> left = Arrays.asList("a", "b", "c");
+    List<String> right = Arrays.asList("a", "b", "b");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("a", "a", "b", "b", "b", "c");
+    assertEquals(expected, merged);
+  }
 }
 
 // hello world
--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar:grading-area'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -8,12 +8,10 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
-cd student-submission
-
 total_tests=1
 passed_tests=0
 
-if [[ ! -f ./ListExamples.java ]]
+if [[ ! -f student-submission/ListExamples.java ]]
 then 
     echo "ListExamples.java does not exist!"
     echo "Passed $passed_tests / $total_tests"
@@ -22,8 +20,11 @@ else
     passed_tests=$(($passed_tests + 1))
 fi 
 
+javac -cp $CPATH student-submission/ListExamples.java -d grading-area
 
+junit_results=`java -cp $CPATH org.junit.runner.JUnitCore ListExamples`
 
+echo $junit_results
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
